cf0d8c935e7eac937f841905ac88dceae43ab9c0
Nikhil1357 <lakshmim1357@gmail.com>
2024-06-26 19:48:58 +0530

    Invalid

--- frontend/Login.js
+++ frontend/Login.js
@@ -29,7 +29,7 @@ export default function Login() {
           },
         ]);
       } else {
-        Alert.alert('Notice', 'Invalid Logins!', [{ text: 'Close' }]);
+        Alert.alert('Notice', 'Invalid!', [{ text: 'Close' }]);
       }
     } catch (err) {
       console.error('Login error:', err.message);
